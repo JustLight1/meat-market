fc73995f07e731a6121938e8acd44d9e89962af6
Форов Александр <5h9t5@list.ru>
2024-10-14 18:34:27 +0300

    feat(products): add admin

---
+++ meat/products/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from products.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'description')
+    search_fields = ('id', 'name')
+    list_editable = ('category',)
+    autocomplete_fields = ('category',)
