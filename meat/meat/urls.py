f30fc47ff0fd32060f7e8786ff670ad528184372
Форов Александр <5h9t5@list.ru>
2024-10-21 19:48:44 +0300

    feat(meat/urls): add handlers, debug and products

--- meat/meat/urls.py
+++ meat/meat/urls.py
@@ -17,14 +17,21 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('', include('products.urls', namespace='products')),
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.internal_server_error'
 
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
